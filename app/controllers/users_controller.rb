ca5c8391a3123b2fbb5eedc0f6415190d2d8240a
Dutch Boyd <dutch@dutchboyd.com>
2014-03-10 03:27:36 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
 	@user = User.find(params[:id])
   end
 
-def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Seal Stakes! Now go make some coin."
       redirect_to @user
     else
