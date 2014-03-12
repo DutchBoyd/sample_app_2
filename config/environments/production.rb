5b18ef83b443853705819de034d0d62f60028bbf
Dutch Boyd <dutch@dutchboyd.com>
2014-03-12 05:45:05 -0500

    Changed production config file to get heroku working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ SampleApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Changing this to get Heroku working with my asset pipeline - db, 3/12/14
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
